f793031cfe03e6ca3309a2fb41d7ace8436279c5
cruisek4n <cruisek4n@yahoo.co.jp>
2021-01-30 17:18:23 +0900

    ユーザー管理機能の可読性とパーフォマンスのために修正点

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,15 +6,17 @@ class User < ApplicationRecord
                                  message: 'Include both letters and numbers' }
 
   with_options presence: true do
-    validates :nickname, uniqueness: true, length: { maximum: 40 }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,
-                                    message: 'Full-width characters' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,
-                                     message: 'Full-width characters' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/,
-                                         message: 'Full-width katakana characters' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/,
-                                          message: 'Full-width katakana characters' }
-    validates :birth_date
+   validates :nickname, uniqueness: true, length: { maximum: 40 }
+   validates :birth_date
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,
+      message: 'Full-width characters' } do
+    validates :last_name
+    validates :first_name 
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/,
+      message: 'Full-width katakana characters' } do
+    validates :last_name_kana 
+    validates :first_name_kana
+    end
   end
 end
